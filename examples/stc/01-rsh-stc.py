a425345e360876f72d36f2af4587035672957203
Gengzhi Yang <genzyang17@gmail.com>
2026-09-03 18:00:51 -0400

    add one example

--- examples/stc/01-rsh-stc.py
+++ examples/stc/01-rsh-stc.py
@@ -1,15 +1,13 @@
 import sys
 import numpy
+import argparse
+
 import pyscf
 from pyscf import lib
-from pyscf.pbc import gto, scf, df
-from pyscf.pbc.tools.k2gamma import translation_vectors_for_kmesh
-from pyscf.gto.moleintor import getints4c, getints
-import copy
-import time
-import os 
-import ctypes
-import argparse
+from pyscf.pbc import gto, scf
+from pyscf.pbc.df.fft_stc import FFTDF_STC
+from pyscf.pbc.df.fft import FFTDF
+from pyscf.pbc.df.rsdf import RSGDF
 
 numpy.set_printoptions(threshold=numpy.inf, linewidth=numpy.inf)
 numpy.set_printoptions(suppress=True, precision=8)
@@ -18,6 +16,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--ke-cutoff", type=int, default=30)
     parser.add_argument("--kmesh", type=int, nargs=3, default=[1, 1, 1])
+    parser.add_argument("--odr", type=float, default=4.)
     args = parser.parse_args()
 
     atom = '''
@@ -30,9 +29,19 @@ if __name__ == "__main__":
         [1.7835000000, 0.0000000000, 1.7835000000]
     ])
 
+    #atom = '''
+    #Si 0 0 0
+    #Si 1.9200424 1.10853699 0.78385403
+    #'''
+    #a = numpy.asarray([
+    #    [3.84008327, 0, 0],
+    #    [1.92004163, 3.32560966, 0],
+    #    [1.92004163, 1.10853655, 3.13541486]
+    #])
+
 
     basis = f'./GTHbasis/minao_gth.dat'
-    pseudo = 'gth-pbe'
+    pseudo = 'gth-hf-rev'
 
     kmesh = numpy.asarray(args.kmesh) # mesh in reciprocal space
     kmesh_label = "_".join(str(k) for k in kmesh)
@@ -46,11 +55,26 @@ if __name__ == "__main__":
     #cell.precision=1e-14
     cell.build()
     kpts = cell.make_kpts(kmesh, time_reversal_symmetry=False)
-    kmf = pyscf.pbc.scf.KRHF(cell,kpts=kpts, exxdiv='stc_ws_3')
-    kmf.with_df = df.FFTDF(cell, kpts=kpts)
+
+    kmf = pyscf.pbc.scf.KRKS(cell, kpts=kpts, exxdiv='ewald')
+    kmf.with_df = RSGDF(cell, kpts=kpts)
     kmf.kernel()
     dm = kmf.make_rdm1()
-    vj, vk = kmf.get_jk(dm_kpts=dm)
+
+
+    kmf_fft = pyscf.pbc.scf.KRHF(cell, kpts=kpts, exxdiv='ewald')
+    kmf_fft.with_df = FFTDF_STC(cell, kpts=kpts)
+    vj, vk = kmf.get_jk(dm_kpts=dm, with_j = False, with_k = True, omega = 0.2)
+
     exchange_energy = - 0.25 * numpy.einsum('kij,kji -> ', dm, vk) / numpy.prod(numpy.array(kmesh))
-    print("Exchange Energy by DM K original: ", exchange_energy)
+    print("Exchange Energy by FFTDF: ", exchange_energy)
+
+    kmf_stc = pyscf.pbc.scf.KRHF(cell, kpts=kpts, exxdiv='vcut_ws')
+    kmf_stc.with_df = FFTDF_STC(cell, kpts=kpts)
+    vj_stc, vk_stc = kmf_stc.get_jk(dm_kpts=dm, with_j = False, with_k = True, omega = 0.2)
+
+    exchange_energy = - 0.25 * numpy.einsum('kij,kji -> ', dm, vk_stc) / numpy.prod(numpy.array(kmesh))
+    print("Exchange Energy by FFTDF_STC: ", exchange_energy)
+
+    print(numpy.linalg.norm(vk - vk_stc)/numpy.linalg.norm(vk))
 
