fixed some bugs, numerical stability issue in smooth_vcut_sph still exists

pyscf/pbc/df/rsdf.py
@@ -48,7 +48,7 @@ import numpy as np
 from pyscf import lib
 from pyscf.lib import logger, zdotCN
 from pyscf.lib import parameters as param
-from pyscf.pbc.df.df import GDF
+from pyscf.pbc.df.df import GDF, AFTDF
 from pyscf.pbc.df import aft, aft_jk
 from pyscf.pbc.df import ft_ao
 from pyscf.pbc.df import rsdf_helper
@@ -146,7 +146,7 @@ cell.dimension=3 with large vacuum.""")
             self.omega = self.omega_dot_Rc / Rc # this actually only affects the SR branch. LR is set by the scf obj exxdiv
             self.omega_j2c = self.omega # this actually only affects the SR branch
         if self.exxdiv == 'smooth_vcut_sph':
-            Rc = (3*nkpts*cell.vol/(4*np.pi))**(1./3)
+            Rc = (3*len(kpts)*cell.vol/(4*np.pi))**(1./3)
             self.omega = self.omega_dot_Rc / Rc # this actually only affects the SR branch. LR is set by the scf obj exxdiv
             self.omega_j2c = self.omega # this actually only affects the SR branch
 
@@ -161,9 +161,15 @@ cell.dimension=3 with large vacuum.""")
     def get_jk(self, dm, hermi=1, kpts=None, kpts_band=None,
                with_j=True, with_k=True, omega=None, exxdiv=None):
 
-        if omega is not None and omega != 0:  # J/K for RSH functionals
-            return GDF.get_jk(self, dm, hermi, kpts, kpts_band, with_j, with_k,
-                                 omega=omega, exxdiv=exxdiv) # folds back to aftdf
+        if omega is not None and omega > 1e-9:  # J/K for RSH functionals
+
+            aftdf_df = AFTDF(self.cell, self.kpts)
+            aftdf_df.omega_dot_Rc = self.omega_dot_Rc
+            aftdf_df.stdout = self.stdout
+            aftdf_df.verbose = self.verbose
+            aftdf_df.max_memory = self.max_memory
+            return aftdf_df.get_jk(self, dm, hermi, kpts, kpts_band, with_j, with_k,
+                                 omega=omega, exxdiv=exxdiv) # GDF folds back to AFTDF
 
         from pyscf.pbc.df.aft import _check_kpts
         from pyscf.pbc.df import df_jk

pyscf/pbc/tools/pbc.py
@@ -374,12 +374,41 @@ def get_coulG(cell, k=np.zeros(3), exx=False, mf=None, mesh=None, Gv=None,
     Nk = len(kpts)
 
     if exxdiv == 'vcut_sph':  # PRB 77 193110
-        Rc = (3*Nk*cell.vol/(4*np.pi))**(1./3)
-        with np.errstate(divide='ignore',invalid='ignore'):
-            coulG = 4*np.pi/absG2*(1.0 - np.cos(np.sqrt(absG2)*Rc))
-        coulG[absG2==0] = 4*np.pi*0.5*Rc**2
 
-        if cell.dimension < 3:
+        if abs(_omega) < 1e-9:
+            Rc = (3*Nk*cell.vol/(4*np.pi))**(1./3)
+            with np.errstate(divide='ignore',invalid='ignore'):
+                coulG = 4*np.pi/absG2*(1.0 - np.cos(np.sqrt(absG2)*Rc))
+            coulG[absG2==0] = 4*np.pi*0.5*Rc**2
+
+            if cell.dimension < 3:
+                raise NotImplementedError
+        elif _omega >= 1e-9:
+            # this should be numerical unstable
+            from scipy.special import erf
+
+            q = np.sqrt(absG2)
+
+            with np.errstate(divide='ignore',invalid='ignore'):
+                coulG = (-4*np.pi/absG2) * (erf(_omega * Rc) * np.cos(q * Rc)) + \
+                        (2*np.pi/absG2) * np.exp(-absG2/(4 * _omega**2)) * (erf(_omega*(Rc + 1j * q/(2*_omega**2))) + erf(_omega*(Rc - 1j * q/(2*_omega**2))))
+            erfVal = erf(_omega * Rc)
+            coulG[absG2==0] = 2 * np.pi * (erfVal * Rc**2 - erfVal / (2 * _omega**2) + _omega * Rc * np.exp(- (_omega * Rc) ** 2) / (_omega**2 * np.sqrt(np.pi)))
+
+            f = np.exp(-absG2*0.25/(omega_stc)**2.)
+
+            v0 = coulG[absG2==0]
+            coulG *= f
+
+            with np.errstate(divide='ignore',invalid='ignore'):
+                coulG += 4*np.pi*(np.exp(-absG2*0.25/(_omega)**2.))/absG2 * (1. - f)
+
+            coulG[absG2==0] = v0 + np.pi/(omega_stc)**2.
+
+            if cell.dimension < 3:
+                raise NotImplementedError
+
+        else:
             raise NotImplementedError
 
     elif exxdiv == 'vcut_ws':  # PRB 87, 165122
@@ -496,8 +525,8 @@ def get_coulG(cell, k=np.zeros(3), exx=False, mf=None, mesh=None, Gv=None,
 
         assert (cell.dimension == 3)
 
+        Rc = (3*len(kpts)*cell.vol/(4*np.pi))**(1./3)
         if omega_stc is None:
-            Rc = (3*len(kpts)*cell.vol/(4*np.pi))**(1./3)
             omega_stc = mf.omega_dot_Rc / Rc
 
         if abs(_omega) < 1e-9:
@@ -522,12 +551,15 @@ def get_coulG(cell, k=np.zeros(3), exx=False, mf=None, mesh=None, Gv=None,
                 raise NotImplementedError
 
         elif _omega >= 1e-9:
+            # this should be numerical unstable
 
             from scipy.special import erf
 
+            q = np.sqrt(absG2)
+
             with np.errstate(divide='ignore',invalid='ignore'):
-                coulG = (-4*np.pi/absG2) * (erf(_omega * Rc) * np.cos(kG * Rc)) + \
-                        (2*np.pi/absG2) * np.exp(-absG2/(4 * _omega**2)) * (erf(_omega*(Rc + 1j * kG/(2*_omega**2))) + erf(_omega*(Rc - 1j * kG/(2*_omega**2))))
+                coulG = (-4*np.pi/absG2) * (erf(_omega * Rc) * np.cos(q * Rc)) + \
+                        (2*np.pi/absG2) * np.exp(-absG2/(4 * _omega**2)) * (erf(_omega*(Rc + 1j * q/(2*_omega**2))) + erf(_omega*(Rc - 1j * q/(2*_omega**2))))
             erfVal = erf(_omega * Rc)
             coulG[absG2==0] = 2 * np.pi * (erfVal * Rc**2 - erfVal / (2 * _omega**2) + _omega * Rc * np.exp(- (_omega * Rc) ** 2) / (_omega**2 * np.sqrt(np.pi)))
 
@@ -543,16 +575,17 @@ def get_coulG(cell, k=np.zeros(3), exx=False, mf=None, mesh=None, Gv=None,
 
             if cell.dimension < 3:
                 raise NotImplementedError
+        else:
+            raise NotImplementedError
 
     elif exxdiv == 'smooth_vcut_ws':  # arXiv 2609.00203
 
         assert (cell.dimension == 3)
+        from pyscf.pbc.lo.base import get_kmesh
+        kmesh = get_kmesh(cell, kpts)
+        Rc = get_ws_inradius(cell.lattice_vectors(), kmesh)
 
         if omega_stc is None:
-            from pyscf.pbc.lo.base import get_kmesh
-            kmesh = get_kmesh(cell, kpts)
-            #log.warn('Using kmesh= %s to calculate WS-inradius Rc', kmesh)
-            Rc = get_ws_inradius(cell.lattice_vectors(), kmesh)
             omega_stc = mf.omega_dot_Rc / Rc
 
         if abs(_omega) < 1e-9:
@@ -744,7 +777,7 @@ def get_coulG(cell, k=np.zeros(3), exx=False, mf=None, mesh=None, Gv=None,
     if _omega != 0 and cell.dimension != 3:
         logger.warn(cell, 'The coulG kernel for range-separated Coulomb potential '
                     f'for PBC {cell.dimension} is inaccurate.')
-    if _omega > 0 and exxdiv not in ('smooth_vcut_ws', 'smooth_vcut_sph'):
+    if _omega > 0 and exxdiv not in ('smooth_vcut_ws', 'smooth_vcut_sph', 'vcut_ws', 'vcut_sph'):
         # note this is the correct LR kernel in reciprocal space
         coulG *= np.exp(-.25/_omega**2 * absG2)
     elif _omega < 0: